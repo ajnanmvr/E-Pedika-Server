5178aa647ecf09b6efb025e97e71a0151f99128c
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-11-28 12:35:16 +0530

    remove unused imports

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const DataModel = require("../models/dataModel");
-const CategoryModel = require("../models/categoryModel");
 const { protect, isAdmin } = require("../utils/authMiddleware");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
@@ -98,7 +97,7 @@ router.get("/type/suggestion", async (req, res, next) => {
 // Read a single data model by ID
 router.get("/i/:id", async (req, res, next) => {
   try {
-    const data = await DataModel.findById(req.params.id).populate('category')
+    const data = await DataModel.findById(req.params.id).populate("category");
     if (!data) {
       res.status(404).json({ error: "Data not found" });
     } else {
@@ -111,7 +110,9 @@ router.get("/i/:id", async (req, res, next) => {
 // Read a single data model by slug
 router.get("/s/:slug", async (req, res, next) => {
   try {
-    const data = await DataModel.findOne({ slug: req.params.slug }).populate('category');
+    const data = await DataModel.findOne({ slug: req.params.slug }).populate(
+      "category"
+    );
     if (!data) {
       res.status(404).json({ error: "Data not found" });
     } else {
